62c71b7882211539d104f3c1dfc8698a8a7946ab
pankona <yosuke.akatsuka@gmail.com>
2015-08-12 23:29:42 +0900

    add answer for 70

---
+++ 70/70.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"code.google.com/p/go-tour/tree"
+	"fmt"
+)
+
+func walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+
+	walk(t.Left, ch)
+	ch <- t.Value
+	walk(t.Right, ch)
+}
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	walk(t, ch)
+	close(ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for v1 := range ch1 {
+		v2 := <-ch2
+		if v1 != v2 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	ch := make(chan int)
+	go Walk(tree.New(1), ch)
+	for v := range ch {
+		fmt.Println(v)
+	}
+
+	if Same(tree.New(1), tree.New(1)) {
+		fmt.Println("ok")
+	} else {
+		fmt.Println("ng")
+	}
+
+	if !Same(tree.New(1), tree.New(2)) {
+		fmt.Println("ok")
+	} else {
+		fmt.Println("ng")
+	}
+}
